a0c06a331550b53d914b030dc0822a8f87b38b77
nikaNGC224 <veronika_rassokhina@mail.ru>
2023-11-02 22:40:41 +0300

    Update file.go

--- findKthLargest/file.go
+++ findKthLargest/file.go
@@ -1,20 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"sort"
+)
+
 func findKthLargest(nums []int, k int) int {
 	if nums == nil {
-		panic("Массив пуст")
+		fmt.Println("Массив пуст")
+		os.Exit(1)
 	}
-	if k > len(nums) {
-		panic("k больше длины массива")
-	}
-	n := len(nums) - 1
-	for i := n; i > k; i-- {
-		for j := n; j > n-i; j-- {
-			if nums[j] > nums[j-1] {
-				nums[j], nums[j-1] = nums[j-1], nums[j]
-			}
-		}
+	if k > len(nums) || k < 1 {
+		fmt.Println("k имеет некорректную величину")
+		os.Exit(2)
 	}
 
-	return nums[k-1]
+	n := len(nums)
+
+	if k > n/2 {
+		lowLimit := k - 1
+		for i := n; i > lowLimit; i-- {
+			for j := n - 1; j > n-i; j-- {
+				if nums[j] < nums[j-1] {
+					nums[j], nums[j-1] = nums[j-1], nums[j]
+				}
+			}
+		}
+		return nums[n-k]
+	} else {
+		lowLimit := n - k
+		for i := n; i > lowLimit; i-- {
+			for j := n - 1; j > n-i; j-- {
+				if nums[j] > nums[j-1] {
+					nums[j], nums[j-1] = nums[j-1], nums[j]
+				}
+			}
+		}
+		return nums[k-1]
+	}
+}
+
+func main() {
+	a := []int{10, 63, 55, 47, 84, 70, 41, 99, 6}
+
+	for k := 1; k <= len(a); k++ {
+		num := findKthLargest(a, k)
+		fmt.Println(num)
+	}
+
+	sort.Slice(a, func(i, j int) bool {
+		return a[i] > a[j]
+	})
+	fmt.Println(a)
 }
