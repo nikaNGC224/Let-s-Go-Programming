4609bc753935368caa8c4efcfa56e67956e9cc33
nikaNGC224 <veronika_rassokhina@mail.ru>
2023-11-02 23:57:18 +0300

    Update file.go
    Update file.go:
    - Своя частичная сортировка с компаратором вынесена в отдельную функцию;
    - Добавлена функция test для наглядного тестирования.

--- findKthLargest/file.go
+++ findKthLargest/file.go
@@ -6,6 +6,18 @@ import (
 	"sort"
 )
 
+// func partialSort: отсортирует k чисел в начало массива
+func partialSort(a []int, k int, less func(e1, e2 int) bool) {
+	n := len(a)
+	for i := n; i > k; i-- {
+		for j := n - 1; j > n-i; j-- {
+			if less(a[j], a[j-1]) {
+				a[j], a[j-1] = a[j-1], a[j]
+			}
+		}
+	}
+}
+
 func findKthLargest(nums []int, k int) int {
 	if nums == nil {
 		fmt.Println("Массив пуст")
@@ -17,40 +29,38 @@ func findKthLargest(nums []int, k int) int {
 	}
 
 	n := len(nums)
+	numsToSort := n - k
 
 	if k > n/2 {
-		lowLimit := k - 1
-		for i := n; i > lowLimit; i-- {
-			for j := n - 1; j > n-i; j-- {
-				if nums[j] < nums[j-1] {
-					nums[j], nums[j-1] = nums[j-1], nums[j]
-				}
-			}
-		}
+		numsToSort = k - 1
+		partialSort(nums, numsToSort, func(e1, e2 int) bool {
+			return e1 < e2
+		})
 		return nums[n-k]
 	} else {
-		lowLimit := n - k
-		for i := n; i > lowLimit; i-- {
-			for j := n - 1; j > n-i; j-- {
-				if nums[j] > nums[j-1] {
-					nums[j], nums[j-1] = nums[j-1], nums[j]
-				}
-			}
-		}
+		partialSort(nums, numsToSort, func(e1, e2 int) bool {
+			return e1 > e2
+		})
 		return nums[k-1]
 	}
 }
 
-func main() {
-	a := []int{10, 63, 55, 47, 84, 70, 41, 99, 6}
-
-	for k := 1; k <= len(a); k++ {
-		num := findKthLargest(a, k)
-		fmt.Println(num)
-	}
+func test(a []int, k int) {
+	fmt.Println("nums:", a, "k =", k)
+	num := findKthLargest(a, k)
+	fmt.Println("result =", num)
 
 	sort.Slice(a, func(i, j int) bool {
 		return a[i] > a[j]
 	})
-	fmt.Println(a)
+	fmt.Println("Sorted array:", a)
+	fmt.Println()
+}
+
+func main() {
+	test([]int{10, 63, 55, 47, 84, 70, 41, 99, 6}, 2)
+
+	test([]int{44, 7, 15, 84, 83, 73, 29, 77, 2}, 5)
+
+	test([]int{42, 48, 19, 99, 50, 90, 80, 24, 10}, 8)
 }
